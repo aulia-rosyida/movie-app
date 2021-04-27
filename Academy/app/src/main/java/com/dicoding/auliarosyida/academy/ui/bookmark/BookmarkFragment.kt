1921b5f30a3bc023afb41dfe2d9080d4340d1277
aulia-rosyida <rosyidaaulia24@gmail.com>
2021-04-27 17:20:09 +0700

    connect bookmark fragment with recyclerView

--- Academy/app/src/main/java/com/dicoding/auliarosyida/academy/ui/bookmark/BookmarkFragment.kt
+++ Academy/app/src/main/java/com/dicoding/auliarosyida/academy/ui/bookmark/BookmarkFragment.kt
@@ -6,27 +6,52 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.core.app.ShareCompat
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.dicoding.auliarosyida.academy.R
+import com.dicoding.auliarosyida.academy.data.CourseEntity
+import com.dicoding.auliarosyida.academy.databinding.FragmentBookmarkBinding
+import com.dicoding.auliarosyida.academy.utils.DataDummy
 
-/**
- * A simple [Fragment] subclass as the second destination in the navigation.
- */
-class BookmarkFragment : Fragment() {
+class BookmarkFragment : Fragment(), BookmarkFragmentCallback {
+
+    lateinit var fragmentBookmarkBinding: FragmentBookmarkBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_bookmark, container, false)
+        fragmentBookmarkBinding = FragmentBookmarkBinding.inflate(inflater, container, false)
+        return fragmentBookmarkBinding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        view.findViewById<Button>(R.id.button_second).setOnClickListener {
-            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
+        if (activity != null) {
+            val courses = DataDummy.generateDummyCourses()
+            val adapter = BookmarkAdapter(this)
+            adapter.setCourses(courses)
+            with(fragmentBookmarkBinding.rvBookmark) {
+                layoutManager = LinearLayoutManager(context)
+                setHasFixedSize(true)
+                this.adapter = adapter
+            }
         }
     }
+
+    override fun onShareClick(course: CourseEntity) {
+        if (activity != null) {
+            val mimeType = "text/plain"
+            ShareCompat.IntentBuilder
+                    .from(requireActivity())
+                    .setType(mimeType)
+                    .setChooserTitle("Bagikan aplikasi ini sekarang.")
+                    .setText(resources.getString(R.string.share_text, course.title))
+                    .startChooser()
+        }
+    }
+
 }
