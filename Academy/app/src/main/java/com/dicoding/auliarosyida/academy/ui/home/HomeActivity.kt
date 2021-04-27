cfbb1ccc6984cb6af9428ac9b4a3b27e761b717d
aulia-rosyida <rosyidaaulia24@gmail.com>
2021-04-27 17:26:20 +0700

    hubungkan academy fragment dan bookmark fragment dg homeActivity

--- Academy/app/src/main/java/com/dicoding/auliarosyida/academy/ui/home/HomeActivity.kt
+++ Academy/app/src/main/java/com/dicoding/auliarosyida/academy/ui/home/HomeActivity.kt
@@ -3,10 +3,19 @@ package com.dicoding.auliarosyida.academy.ui.home
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.dicoding.auliarosyida.academy.R
+import com.dicoding.auliarosyida.academy.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+
+        val activityHomeBinding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(activityHomeBinding.root)
+
+        val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
+        activityHomeBinding.viewPager.adapter = sectionsPagerAdapter
+        activityHomeBinding.tabs.setupWithViewPager(activityHomeBinding.viewPager)
+        supportActionBar?.elevation = 0f
+
     }
 }
