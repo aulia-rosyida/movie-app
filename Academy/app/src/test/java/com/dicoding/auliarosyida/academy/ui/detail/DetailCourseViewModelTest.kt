58e4e0663b414a4905173a2939203367c85c1985
aulia-rosyida <rosyidaaulia24@gmail.com>
2021-04-27 23:52:48 +0700

    uji DetailCourseViewModel

--- Academy/app/src/test/java/com/dicoding/auliarosyida/academy/ui/detail/DetailCourseViewModelTest.kt
+++ Academy/app/src/test/java/com/dicoding/auliarosyida/academy/ui/detail/DetailCourseViewModelTest.kt
@@ -1,12 +1,44 @@
 package com.dicoding.auliarosyida.academy.ui.detail
 
-import junit.framework.TestCase
+import com.dicoding.auliarosyida.academy.utils.DataDummy
+import junit.framework.TestCase.assertEquals
+import junit.framework.TestCase.assertNotNull
+import org.junit.Before
+import org.junit.Test
 
-class DetailCourseViewModelTest : TestCase() {
+class DetailCourseViewModelTest {
+    private lateinit var viewModel: DetailCourseViewModel
+    private val dummyCourse = DataDummy.generateDummyCourses()[0]
+    private val courseId = dummyCourse.courseId
 
-    fun testSetSelectedCourse() {}
+    @Before
+    fun setUp() {
+        viewModel = DetailCourseViewModel()
+        viewModel.setSelectedCourse(courseId)
+    }
 
-    fun testGetCourse() {}
+    /**
+     * membandingkan data CourseEntity hasil dari viewModel.getCourses().
+     * */
+    @Test
+    fun getCourse() {
+        viewModel.setSelectedCourse(dummyCourse.courseId)
+        val courseEntity = viewModel.getCourse()
+        assertNotNull(courseEntity)
+        assertEquals(dummyCourse.courseId, courseEntity.courseId)
+        assertEquals(dummyCourse.deadline, courseEntity.deadline)
+        assertEquals(dummyCourse.description, courseEntity.description)
+        assertEquals(dummyCourse.imagePath, courseEntity.imagePath)
+        assertEquals(dummyCourse.title, courseEntity.title)
+    }
 
-    fun testGetModules() {}
+    /**
+     *menguji DetailCourseViewModel dengan cara membandingkan ukuran dari array viewModel.getModules()
+     * */
+    @Test
+    fun getModules() {
+        val moduleEntities = viewModel.getModules()
+        assertNotNull(moduleEntities)
+        assertEquals(7, moduleEntities.size.toLong())
+    }
 }
