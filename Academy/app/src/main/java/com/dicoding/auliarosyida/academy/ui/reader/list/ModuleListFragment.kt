88ec055b978dc68791447e1e2f4700931617878f
aulia-rosyida <rosyidaaulia24@gmail.com>
2021-04-27 17:33:15 +0700

    connect ModuleListFragment dg recyclerView

--- Academy/app/src/main/java/com/dicoding/auliarosyida/academy/ui/reader/list/ModuleListFragment.kt
+++ Academy/app/src/main/java/com/dicoding/auliarosyida/academy/ui/reader/list/ModuleListFragment.kt
@@ -1,60 +1,65 @@
 package com.dicoding.auliarosyida.academy.ui.reader.list
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.dicoding.auliarosyida.academy.R
+import com.dicoding.auliarosyida.academy.data.ModuleEntity
+import com.dicoding.auliarosyida.academy.databinding.FragmentModuleListBinding
+import com.dicoding.auliarosyida.academy.ui.reader.CourseReaderActivity
+import com.dicoding.auliarosyida.academy.ui.reader.CourseReaderCallback
+import com.dicoding.auliarosyida.academy.utils.DataDummy
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+class ModuleListFragment : Fragment() , MyAdapterClickListener {
 
-/**
- * A simple [Fragment] subclass.
- * Use the [ModuleListFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class ModuleListFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    companion object {
+        val TAG: String = ModuleListFragment::class.java.simpleName
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+        fun newInstance(): ModuleListFragment = ModuleListFragment()
     }
+
+    private lateinit var fragmentModuleListBinding: FragmentModuleListBinding
+    private lateinit var adapter: ModuleListAdapter
+    private lateinit var courseReaderCallback: CourseReaderCallback
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_module_list, container, false)
+        fragmentModuleListBinding = FragmentModuleListBinding.inflate(inflater, container, false)
+        return fragmentModuleListBinding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ModuleListFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ModuleListFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        adapter = ModuleListAdapter(this)
+        populateRecyclerView(DataDummy.generateDummyModules("a14"))
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        courseReaderCallback = context as CourseReaderActivity
+    }
+
+    override fun onItemClicked(position: Int, moduleId: String) {
+        courseReaderCallback.moveTo(position, moduleId)
+    }
+
+    private fun populateRecyclerView(modules: List<ModuleEntity>) {
+        with(fragmentModuleListBinding) {
+            progressBar.visibility = View.GONE
+            adapter.setModules(modules)
+            rvModule.layoutManager = LinearLayoutManager(context)
+            rvModule.setHasFixedSize(true)
+            rvModule.adapter = adapter
+            val dividerItemDecoration = DividerItemDecoration(requireContext(), DividerItemDecoration.VERTICAL)
+            rvModule.addItemDecoration(dividerItemDecoration)
+        }
     }
 }
