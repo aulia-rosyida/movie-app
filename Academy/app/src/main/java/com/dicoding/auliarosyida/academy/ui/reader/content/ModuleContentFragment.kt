66cf507ff48ade4491a2cc360d90e46e8479307a
aulia-rosyida <rosyidaaulia24@gmail.com>
2021-04-27 17:28:45 +0700

    tampilkan data dummy di webView

--- Academy/app/src/main/java/com/dicoding/auliarosyida/academy/ui/reader/content/ModuleContentFragment.kt
+++ Academy/app/src/main/java/com/dicoding/auliarosyida/academy/ui/reader/content/ModuleContentFragment.kt
@@ -6,55 +6,36 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.dicoding.auliarosyida.academy.R
+import com.dicoding.auliarosyida.academy.data.ContentEntity
+import com.dicoding.auliarosyida.academy.databinding.FragmentModuleContentBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [ModuleContentFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ModuleContentFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+    companion object {
+        val TAG: String = ModuleContentFragment::class.java.simpleName
+        fun newInstance(): ModuleContentFragment = ModuleContentFragment()
     }
+
+    private lateinit var fragmentModuleContentBinding: FragmentModuleContentBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_module_content, container, false)
+        fragmentModuleContentBinding = FragmentModuleContentBinding.inflate(inflater, container, false)
+        return fragmentModuleContentBinding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ModuleContentFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ModuleContentFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        if (activity != null) {
+            val content = ContentEntity("<h3 class=\\\"fr-text-bordered\\\">Contoh Content</h3><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>")
+            populateWebView(content)
+        }
+    }
+
+    private fun populateWebView(content: ContentEntity) {
+        fragmentModuleContentBinding.webView.loadData(content.content ?: "", "text/html", "UTF-8")
     }
 }
