8dece51b8bd5b5fb2698bb33657b53c6e958ce33
aulia-rosyida <rosyidaaulia24@gmail.com>
2021-04-27 17:23:57 +0700

    hubungkan detail course activity dengan recyclerView

--- Academy/app/src/main/java/com/dicoding/auliarosyida/academy/ui/detail/DetailCourseActivity.kt
+++ Academy/app/src/main/java/com/dicoding/auliarosyida/academy/ui/detail/DetailCourseActivity.kt
@@ -1,12 +1,21 @@
 package com.dicoding.auliarosyida.academy.ui.detail
 
+import android.content.Intent
 import android.os.Bundle
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.bumptech.glide.request.RequestOptions
 import com.dicoding.auliarosyida.academy.R
+import com.dicoding.auliarosyida.academy.data.CourseEntity
 import com.dicoding.auliarosyida.academy.databinding.ActivityDetailCourseBinding
 import com.dicoding.auliarosyida.academy.databinding.ContentDetailCourseBinding
+import com.dicoding.auliarosyida.academy.ui.reader.CourseReaderActivity
+import com.dicoding.auliarosyida.academy.utils.DataDummy
 
 class DetailCourseActivity : AppCompatActivity() {
 
@@ -18,6 +27,7 @@ class DetailCourseActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        
         val activityDetailCourseBinding = ActivityDetailCourseBinding.inflate(layoutInflater)
         detailContentBinding = activityDetailCourseBinding.detailContent
 
@@ -25,5 +35,49 @@ class DetailCourseActivity : AppCompatActivity() {
 
         setSupportActionBar(activityDetailCourseBinding.toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        val adapter = DetailCourseAdapter()
+
+        val extras = intent.extras
+        if (extras != null) {
+            val courseId = extras.getString(EXTRA_COURSE)
+            if (courseId != null) {
+                val modules = DataDummy.generateDummyModules(courseId)
+                adapter.setModules(modules)
+                for (course in DataDummy.generateDummyCourses()) {
+                    if (course.courseId == courseId) {
+                        populateCourse(course)
+                    }
+                }
+            }
+        }
+
+        with(detailContentBinding.rvModule) {
+            isNestedScrollingEnabled = false
+            layoutManager = LinearLayoutManager(this@DetailCourseActivity)
+            setHasFixedSize(true)
+            this.adapter = adapter
+            val dividerItemDecoration = DividerItemDecoration(this.context, DividerItemDecoration.VERTICAL)
+            addItemDecoration(dividerItemDecoration)
+        }
+    }
+
+    private fun populateCourse(courseEntity: CourseEntity) {
+        detailContentBinding.textTitle.text = courseEntity.title
+        detailContentBinding.textDescription.text = courseEntity.description
+        detailContentBinding.textDate.text = resources.getString(R.string.deadline_date, courseEntity.deadline)
+
+        Glide.with(this)
+                .load(courseEntity.imagePath)
+                .transform(RoundedCorners(20))
+                .apply(RequestOptions.placeholderOf(R.drawable.ic_loading)
+                        .error(R.drawable.ic_error))
+                .into(detailContentBinding.imagePoster)
+
+        detailContentBinding.btnStart.setOnClickListener {
+            val intent = Intent(this@DetailCourseActivity, CourseReaderActivity::class.java)
+            intent.putExtra(CourseReaderActivity.EXTRA_COURSE_ID, courseEntity.courseId)
+            startActivity(intent)
+        }
     }
 }
