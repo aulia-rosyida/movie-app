4e0f18742218c0f13cd98a35ef129c8b33757e5d
aulia-rosyida <rosyidaaulia24@gmail.com>
2021-04-27 17:36:42 +0700

    connect ModuleContentFragment and ModuleListFragment with CourseReaderAtivity

--- Academy/app/src/main/java/com/dicoding/auliarosyida/academy/ui/reader/CourseReaderActivity.kt
+++ Academy/app/src/main/java/com/dicoding/auliarosyida/academy/ui/reader/CourseReaderActivity.kt
@@ -3,8 +3,10 @@ package com.dicoding.auliarosyida.academy.ui.reader
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.dicoding.auliarosyida.academy.R
+import com.dicoding.auliarosyida.academy.ui.reader.content.ModuleContentFragment
+import com.dicoding.auliarosyida.academy.ui.reader.list.ModuleListFragment
 
-class CourseReaderActivity : AppCompatActivity() {
+class CourseReaderActivity : AppCompatActivity(), CourseReaderCallback {
 
     companion object {
         const val EXTRA_COURSE_ID = "extra_course_id"
@@ -13,5 +15,42 @@ class CourseReaderActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_course_reader)
+
+        val bundle = intent.extras
+        if (bundle != null) {
+            val courseId = bundle.getString(EXTRA_COURSE_ID)
+            if (courseId != null) {
+                populateFragment()
+            }
+        }
+    }
+
+    /**
+     * Metode moveTo digunakan untuk memanggil ModuleContentFragment sesuai dengan posisi dan moduleId yang dipilih.
+     * */
+    override fun moveTo(position: Int, moduleId: String) {
+        val fragment = ModuleContentFragment.newInstance()
+        supportFragmentManager.beginTransaction().add(R.id.frame_container, fragment, ModuleContentFragment.TAG)
+                .addToBackStack(null)
+                .commit()
+    }
+
+    override fun onBackPressed() {
+        if (supportFragmentManager.backStackEntryCount <= 1) {
+            finish()
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    private fun populateFragment() {
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        var fragment = supportFragmentManager.findFragmentByTag(ModuleListFragment.TAG)
+        if (fragment == null) {
+            fragment = ModuleListFragment.newInstance()
+            fragmentTransaction.add(R.id.frame_container, fragment, ModuleListFragment.TAG)
+            fragmentTransaction.addToBackStack(null)
+        }
+        fragmentTransaction.commit()
     }
 }
