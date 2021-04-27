321576501bae0b9d8e1da67e85a33b6e554bcfe2
aulia-rosyida <rosyidaaulia24@gmail.com>
2021-04-27 23:54:56 +0700

    implement unittesting CourseReaderViewModel

--- Academy/app/src/test/java/com/dicoding/auliarosyida/academy/ui/reader/CourseReaderViewModelTest.kt
+++ Academy/app/src/test/java/com/dicoding/auliarosyida/academy/ui/reader/CourseReaderViewModelTest.kt
@@ -1,14 +1,53 @@
 package com.dicoding.auliarosyida.academy.ui.reader
 
+import com.dicoding.auliarosyida.academy.data.ContentEntity
+import com.dicoding.auliarosyida.academy.utils.DataDummy
 import junit.framework.TestCase
+import junit.framework.TestCase.assertEquals
+import junit.framework.TestCase.assertNotNull
+import org.junit.Before
+import org.junit.Test
 
-class CourseReaderViewModelTest : TestCase() {
+class CourseReaderViewModelTest  {
 
-    fun testSetSelectedCourse() {}
+    private lateinit var viewModel: CourseReaderViewModel
 
-    fun testSetSelectedModule() {}
+    private val dummyCourse = DataDummy.generateDummyCourses()[0]
+    private val courseId = dummyCourse.courseId
+    private val dummyModules = DataDummy.generateDummyModules(courseId)
+    private val moduleId = dummyModules[0].moduleId
 
-    fun testGetModules() {}
+    @Before
+    fun setUp() {
+        viewModel = CourseReaderViewModel()
+        viewModel.setSelectedCourse(courseId)
+        viewModel.setSelectedModule(moduleId)
 
-    fun testGetSelectedModule() {}
+        val dummyModule = dummyModules[0]
+        dummyModule.contentEntity = ContentEntity("<h3 class=\\\"fr-text-bordered\\\">"+dummyModule.title+"</h3><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>")
+    }
+
+    /**
+     * menguji CourseReaderViewModel dengan cara membandingkan ukuran dari array viewModel.getModules()
+     * */
+    @Test
+    fun getModules() {
+        val moduleEntities = viewModel.getModules()
+        assertNotNull(moduleEntities)
+        assertEquals(7, moduleEntities.size.toLong())
+    }
+
+    /**
+     *  membandingkan data ModuleEntity hasil dari viewModel.getSelectedModule().
+     * */
+    @Test
+    fun getSelectedModule() {
+        val moduleEntity = viewModel.getSelectedModule()
+        assertNotNull(moduleEntity)
+        val contentEntity = moduleEntity.contentEntity
+        assertNotNull(contentEntity)
+        val content = contentEntity?.content
+        assertNotNull(content)
+        assertEquals(content, dummyModules[0].contentEntity?.content)
+    }
 }
