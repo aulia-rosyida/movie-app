0b6ee37dc80373f15d01bf3e88ee4f4d49f21504
aulia-rosyida <rosyidaaulia24@gmail.com>
2021-04-28 10:06:45 +0700

    init tvshow fragment

---
+++ MoviesApp/app/src/main/java/com/dicoding/auliarosyida/moviesapp/tvshowtab/TvShowFragment.kt
@@ -0,0 +1,32 @@
+package com.dicoding.auliarosyida.moviesapp.tvshowtab
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.dicoding.auliarosyida.moviesapp.R
+
+class TvShowFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = TvShowFragment()
+    }
+
+    private lateinit var viewModel: TvShowViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.tv_show_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(TvShowViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ MoviesApp/app/src/main/java/com/dicoding/auliarosyida/moviesapp/tvshowtab/TvShowViewModel.kt
@@ -0,0 +1,7 @@
+package com.dicoding.auliarosyida.moviesapp.tvshowtab
+
+import androidx.lifecycle.ViewModel
+
+class TvShowViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
