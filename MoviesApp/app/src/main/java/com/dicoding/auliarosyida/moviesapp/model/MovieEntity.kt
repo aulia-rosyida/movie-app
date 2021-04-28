d708e637d0dc19206ba0d90673a1da912b2c8cb9
aulia-rosyida <rosyidaaulia24@gmail.com>
2021-04-28 10:44:31 +0700

    persiapan asset poster

--- MoviesApp/app/src/main/java/com/dicoding/auliarosyida/moviesapp/model/MovieEntity.kt
+++ MoviesApp/app/src/main/java/com/dicoding/auliarosyida/moviesapp/model/MovieEntity.kt
@@ -1,13 +1,13 @@
-package com.dicoding.auliarosyida.moviesapp.datamodel
+package com.dicoding.auliarosyida.moviesapp.model
 
 data class MovieEntity (
     var id: String,
+    var poster: Int = 0,
     var title: String,
     var description: String,
     var releaseDate: String,
     var genre: String,
     var status: String,
     var duration: String,
-    var originalLanguage: String,
-    var isMovie: Boolean
+    var originalLanguage: String
 )
