b66fb1b2882f6abb599367cef491ce8bc02ac6e5
aulia-rosyida <rosyidaaulia24@gmail.com>
2021-04-28 09:30:11 +0700

    init submisi 1 jetpack

---
+++ MoviesApp/app/src/main/java/com/dicoding/auliarosyida/moviesapp/LandingActivity.kt
@@ -0,0 +1,11 @@
+package com.dicoding.auliarosyida.moviesapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LandingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_landing)
+    }
+}
