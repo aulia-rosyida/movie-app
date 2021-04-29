74f9ba025d56499679c7f5c3363889b363897342
aulia-rosyida <rosyidaaulia24@gmail.com>
2021-04-29 09:46:42 +0700

    siapkan frontend layout

--- MoviesApp/app/src/main/java/com/dicoding/auliarosyida/moviesapp/ui/DetailActivity.kt
+++ MoviesApp/app/src/main/java/com/dicoding/auliarosyida/moviesapp/ui/DetailActivity.kt
@@ -3,10 +3,11 @@ package com.dicoding.auliarosyida.moviesapp.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.dicoding.auliarosyida.moviesapp.databinding.ActivityDetailBinding
+import com.dicoding.auliarosyida.moviesapp.databinding.ContentDetailMovieBinding
 
 class DetailActivity : AppCompatActivity() {
 
-    private lateinit var detailMovieBinding: ActivityDetailBinding
+    private lateinit var detailContentBinding: ContentDetailMovieBinding
 
     companion object {
         const val extraMovie = "extra_movie"
@@ -14,9 +15,12 @@ class DetailActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        detailMovieBinding = ActivityDetailBinding.inflate(layoutInflater)
-        setContentView(detailMovieBinding.root)
+        val activityDetailMovieBinding = ActivityDetailBinding.inflate(layoutInflater)
+        detailContentBinding = activityDetailMovieBinding.detailContent
 
+        setContentView(activityDetailMovieBinding.root)
+
+        setSupportActionBar(activityDetailMovieBinding.toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 }
