f43d53ddcfb047adee2a6af0ba8064208d158a3c
aulia-rosyida <rosyidaaulia24@gmail.com>
2021-04-30 08:26:01 +0700

    kelas movie fragment sudah menampilkan data dari kelas dataMovies

--- MoviesApp/app/src/main/java/com/dicoding/auliarosyida/moviesapp/ui/movietab/MovieFragment.kt
+++ MoviesApp/app/src/main/java/com/dicoding/auliarosyida/moviesapp/ui/movietab/MovieFragment.kt
@@ -6,27 +6,43 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.dicoding.auliarosyida.moviesapp.R
+import com.dicoding.auliarosyida.moviesapp.databinding.MovieFragmentBinding
+import com.dicoding.auliarosyida.moviesapp.utils.DataMovies
 
 class MovieFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = MovieFragment()
-    }
+    private lateinit var movieFragmentBinding: MovieFragmentBinding
 
-    private lateinit var viewModel: MovieViewModel
+//    companion object {
+//        fun newInstance() = MovieFragment()
+//    }
+//
+//    private lateinit var viewModel: MovieViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.movie_fragment, container, false)
+        movieFragmentBinding = MovieFragmentBinding.inflate(layoutInflater, container, false)
+        return movieFragmentBinding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(MovieViewModel::class.java)
-        // TODO: Use the ViewModel
+       // viewModel = ViewModelProvider(this).get(MovieViewModel::class.java)
+        if (activity != null) {
+            val movies = DataMovies.listMovieData
+            val movieAdapter = MovieAdapter()
+            movieAdapter.setMovies(movies)
+
+            with(movieFragmentBinding.rvMovie){
+                layoutManager = LinearLayoutManager(context)
+                setHasFixedSize(true)
+                adapter = movieAdapter
+            }
+        }
     }
 
 }
