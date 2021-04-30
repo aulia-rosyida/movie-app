eeb58463234dfab4b30f5c3f851ad20e6a4f2117
aulia-rosyida <rosyidaaulia24@gmail.com>
2021-04-30 09:01:13 +0700

    connect detail activity

--- MoviesApp/app/src/main/java/com/dicoding/auliarosyida/moviesapp/ui/DetailActivity.kt
+++ MoviesApp/app/src/main/java/com/dicoding/auliarosyida/moviesapp/ui/DetailActivity.kt
@@ -2,8 +2,14 @@ package com.dicoding.auliarosyida.moviesapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.bumptech.glide.request.RequestOptions
+import com.dicoding.auliarosyida.moviesapp.R
 import com.dicoding.auliarosyida.moviesapp.databinding.ActivityDetailBinding
 import com.dicoding.auliarosyida.moviesapp.databinding.ContentDetailMovieBinding
+import com.dicoding.auliarosyida.moviesapp.model.MovieEntity
+import com.dicoding.auliarosyida.moviesapp.utils.DataMovies
 
 class DetailActivity : AppCompatActivity() {
 
@@ -22,5 +28,44 @@ class DetailActivity : AppCompatActivity() {
 
         setSupportActionBar(activityDetailMovieBinding.toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        val extras = intent.extras
+        if (extras != null) {
+            val tempId = extras.getString(extraMovie)
+            if (tempId != null ) {
+                when(tempId.first()) {
+                'm' -> {
+                    for (movie in DataMovies.generateMovies()) {
+                        if (movie.id == tempId) {
+                            populateCard(movie)
+                        }
+                    }}
+                't' -> {
+                    for (tvshow in DataMovies.generateTvShows()) {
+                        if (tvshow.id == tempId) {
+                            populateCard(tvshow)
+                        }
+                    }}
+                }
+            }
+        }
+    }
+
+    private fun populateCard(entity: MovieEntity) {
+        detailContentBinding.textYear.text = entity.releaseYear
+        detailContentBinding.textDuration.text = entity.duration
+        detailContentBinding.textTitle.text = entity.title
+        detailContentBinding.textGenre.text = entity.genre
+
+        Glide.with(this)
+                .load(entity.poster)
+                .transform(RoundedCorners(20))
+                .apply(RequestOptions.placeholderOf(R.drawable.ic_loading)
+                        .error(R.drawable.ic_error))
+                .into(detailContentBinding.imagePoster)
+
+        detailContentBinding.textOverview.text = entity.overview
+        detailContentBinding.textStatus.text = entity.status
+        detailContentBinding.textLang.text = entity.originalLanguage
     }
 }
