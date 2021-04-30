c180be03228c0cecc3009fdf19fe3014bdc52b1a
aulia-rosyida <rosyidaaulia24@gmail.com>
2021-04-30 09:05:30 +0700

    implement pager adapter

--- MoviesApp/app/src/main/java/com/dicoding/auliarosyida/moviesapp/ui/landingpage/LandingActivity.kt
+++ MoviesApp/app/src/main/java/com/dicoding/auliarosyida/moviesapp/ui/landingpage/LandingActivity.kt
@@ -3,10 +3,18 @@ package com.dicoding.auliarosyida.moviesapp.ui.landingpage
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.dicoding.auliarosyida.moviesapp.R
+import com.dicoding.auliarosyida.moviesapp.databinding.ActivityLandingBinding
 
 class LandingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_landing)
+        val activityLandingBinding = ActivityLandingBinding.inflate(layoutInflater)
+        setContentView(activityLandingBinding.root)
+
+        val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
+        activityLandingBinding.viewPager.adapter = sectionsPagerAdapter
+        activityLandingBinding.tabs.setupWithViewPager(activityLandingBinding.viewPager)
+
+        supportActionBar?.elevation = 0f
     }
 }
