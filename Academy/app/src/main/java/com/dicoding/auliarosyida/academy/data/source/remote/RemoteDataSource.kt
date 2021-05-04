75eb74bd6cadd714637d71ea631e591ddf292df4
aulia-rosyida <rosyidaaulia24@gmail.com>
2021-05-04 23:14:39 +0700

    kelas repository untuk remote

---
+++ Academy/app/src/main/java/com/dicoding/auliarosyida/academy/data/source/remote/RemoteDataSource.kt
@@ -0,0 +1,29 @@
+package com.dicoding.auliarosyida.academy.data.source.remote
+
+import com.dicoding.auliarosyida.academy.data.source.remote.response.ContentResponse
+import com.dicoding.auliarosyida.academy.data.source.remote.response.CourseResponse
+import com.dicoding.auliarosyida.academy.data.source.remote.response.ModuleResponse
+import com.dicoding.auliarosyida.academy.utils.JsonHelper
+
+/**
+ *  kelas repository untuk remote
+ * */
+class RemoteDataSource private constructor(private val jsonHelper: JsonHelper) {
+
+    companion object {
+        @Volatile
+        private var instance: RemoteDataSource? = null
+
+        fun getInstance(helper: JsonHelper): RemoteDataSource =
+            instance ?: synchronized(this) {
+                instance ?: RemoteDataSource(helper).apply { instance = this }
+            }
+    }
+
+    fun getAllCourses(): List<CourseResponse> = jsonHelper.loadCourses()
+
+    fun getModules(courseId: String): List<ModuleResponse> = jsonHelper.loadModule(courseId)
+
+    fun getContent(moduleId: String): ContentResponse = jsonHelper.loadContent(moduleId)
+
+}
