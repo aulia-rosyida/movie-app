f09e61d1d68abf18e8047aeffc7e07fc90c2891d
aulia-rosyida <rosyidaaulia24@gmail.com>
2021-05-04 23:28:16 +0700

    tambahkan constructor dan changes code to use acad repository in each ViewModel

--- Academy/app/src/main/java/com/dicoding/auliarosyida/academy/ui/academy/AcademyViewModel.kt
+++ Academy/app/src/main/java/com/dicoding/auliarosyida/academy/ui/academy/AcademyViewModel.kt
@@ -2,8 +2,10 @@ package com.dicoding.auliarosyida.academy.ui.academy
 
 import androidx.lifecycle.ViewModel
 import com.dicoding.auliarosyida.academy.data.CourseEntity
+import com.dicoding.auliarosyida.academy.data.source.AcademyRepository
 import com.dicoding.auliarosyida.academy.utils.DataDummy
 
-class AcademyViewModel : ViewModel(){
-    fun getCourses(): List<CourseEntity> = DataDummy.generateDummyCourses()
+class AcademyViewModel(private val academyRepository: AcademyRepository) : ViewModel(){
+
+    fun getCourses(): List<CourseEntity> =  academyRepository.getAllCourses()
 }
--- Academy/app/src/main/java/com/dicoding/auliarosyida/academy/ui/bookmark/BookmarkViewModel.kt
+++ Academy/app/src/main/java/com/dicoding/auliarosyida/academy/ui/bookmark/BookmarkViewModel.kt
@@ -2,9 +2,10 @@ package com.dicoding.auliarosyida.academy.ui.bookmark
 
 import androidx.lifecycle.ViewModel
 import com.dicoding.auliarosyida.academy.data.CourseEntity
+import com.dicoding.auliarosyida.academy.data.source.AcademyRepository
 import com.dicoding.auliarosyida.academy.utils.DataDummy
 
-class BookmarkViewModel: ViewModel() {
+class BookmarkViewModel(private val academyRepository: AcademyRepository) : ViewModel() {
 
-    fun getBookmarks(): List<CourseEntity> = DataDummy.generateDummyCourses()
+    fun getBookmarks(): List<CourseEntity> = academyRepository.getBookmarkedCourses()
 }
