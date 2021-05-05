c68840a05765e87de4e9fe21d1897670863f85e6
aulia-rosyida <rosyidaaulia24@gmail.com>
2021-05-05 21:41:06 +0700

    call MainViewModel in MainActivity

--- MyLiveData/app/src/main/java/com/dicoding/auliarosyida/mylivedata/MainActivity.kt
+++ MyLiveData/app/src/main/java/com/dicoding/auliarosyida/mylivedata/MainActivity.kt
@@ -2,10 +2,19 @@ package com.dicoding.auliarosyida.mylivedata
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import com.dicoding.auliarosyida.mylivedata.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mLiveDataTimerViewModel: MainViewModel
+    private lateinit var activityMainBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(activityMainBinding.root)
+
+        mLiveDataTimerViewModel = ViewModelProvider(this)[MainViewModel::class.java]
     }
 }
