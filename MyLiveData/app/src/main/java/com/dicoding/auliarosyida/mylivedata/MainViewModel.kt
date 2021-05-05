577e910b2236a435da24e144b456c6b91588a350
aulia-rosyida <rosyidaaulia24@gmail.com>
2021-05-05 21:40:23 +0700

    add liveData object that will subscribed by MainActivity

--- MyLiveData/app/src/main/java/com/dicoding/auliarosyida/mylivedata/MainViewModel.kt
+++ MyLiveData/app/src/main/java/com/dicoding/auliarosyida/mylivedata/MainViewModel.kt
@@ -1,6 +1,8 @@
 package com.dicoding.auliarosyida.mylivedata
 
 import android.os.SystemClock
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import java.util.*
 
@@ -12,12 +14,19 @@ class MainViewModel: ViewModel() {
 
     // metode untuk menjalankan timer di konstruktor
     private val mInitialTime = SystemClock.elapsedRealtime()
+    private val mElapsedTime = MutableLiveData<Long?>() //obyek LiveData yang nantinya akan di-subscribe oleh MainActivity.
+
     init {
         val timer = Timer()
         timer.scheduleAtFixedRate(object : TimerTask() {
             override fun run() {
                 val newValue = (SystemClock.elapsedRealtime() - mInitialTime) / 1000
+                mElapsedTime.postValue(newValue)
             }
         }, ONE_SECOND.toLong(), ONE_SECOND.toLong())
+    }
+
+    fun getElapsedTime(): LiveData<Long?> {
+        return mElapsedTime
     }
 }
