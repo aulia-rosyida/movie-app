d1d336fe17278071556030c5cb7f49c2df1fea45
aulia-rosyida <rosyidaaulia24@gmail.com>
2021-05-05 23:05:33 +0700

    observe livedata dan tampilkan hasil di MainActivity - komponen UI

--- LiveDataWithAPI/app/src/main/java/com/dicoding/auliarosyida/livedatawithapi/MainActivity.kt
+++ LiveDataWithAPI/app/src/main/java/com/dicoding/auliarosyida/livedatawithapi/MainActivity.kt
@@ -2,10 +2,43 @@ package com.dicoding.auliarosyida.livedatawithapi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ArrayAdapter
+import androidx.activity.viewModels
+import com.bumptech.glide.Glide
+import com.dicoding.auliarosyida.livedatawithapi.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val mainViewModel: MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(activityMainBinding.root)
+
+        supportActionBar?.hide()
+
+        // observe LiveData di ViewModel dari MainActivity dan tampilkan hasilnya pada komponen UI
+        mainViewModel.restaurant.observe(this, { restaurant ->
+            activityMainBinding.tvTitle.text = restaurant.name
+            activityMainBinding.tvDescription.text = restaurant.description
+            Glide.with(this)
+                    .load("https://restaurant-api.dicoding.dev/images/large/${restaurant.pictureId}")
+                    .into(activityMainBinding.ivPicture)
+        })
+
+        mainViewModel.listReview.observe(this, { consumerReviews ->
+            val listReview = consumerReviews.map {
+                "${it.review}\n- ${it.name}"
+            }
+            activityMainBinding.lvReview.adapter =
+                    ArrayAdapter(this, R.layout.item_review, listReview)
+            activityMainBinding.edReview.setText("")
+        })
+
+        mainViewModel.isLoading.observe(this, {
+            activityMainBinding.progressBar.visibility = if (it) View.VISIBLE else View.GONE
+        })
     }
 }
