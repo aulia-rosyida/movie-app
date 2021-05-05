d55974bffa695b81024ac53a8aae55e49c6998f5
aulia-rosyida <rosyidaaulia24@gmail.com>
2021-05-05 08:56:46 +0700

    menyiapkan test AcademyRepository

---
+++ Academy/app/src/test/java/com/dicoding/auliarosyida/academy/data/AcademyRepositoryTest.kt
@@ -0,0 +1,20 @@
+package com.dicoding.auliarosyida.academy.data
+
+import junit.framework.TestCase
+
+class AcademyRepositoryTest : TestCase() {
+
+    public override fun setUp() {
+        super.setUp()
+    }
+
+    fun testGetAllCourses() {}
+
+    fun testGetBookmarkedCourses() {}
+
+    fun testGetCourseWithModules() {}
+
+    fun testGetAllModulesByCourse() {}
+
+    fun testGetContent() {}
+}
---
+++ Academy/app/src/test/java/com/dicoding/auliarosyida/academy/data/FakeAcademyRepository.kt
@@ -0,0 +1,97 @@
+package com.dicoding.auliarosyida.academy.data
+
+import com.dicoding.auliarosyida.academy.data.ContentEntity
+import com.dicoding.auliarosyida.academy.data.CourseEntity
+import com.dicoding.auliarosyida.academy.data.ModuleEntity
+import com.dicoding.auliarosyida.academy.data.source.AcademyDataSource
+import com.dicoding.auliarosyida.academy.data.source.remote.RemoteDataSource
+
+/**
+ * kelas untuk menghubungkan RemoteDataSource.
+ * AcademyRepository sebagai filter antara remote dan local.
+ * */
+class FakeAcademyRepository (private val remoteDataSource: RemoteDataSource) : AcademyDataSource {
+
+    /**
+     * metode getAllCourses melakukan perubahan data array dari CourseResponse menjadi CourseEntity.
+     * Ini dilakukan agar apa yang ada di View tidak banyak berubah.
+     *
+     * Sehingga di dalam AcademyViewModel bisa langsung memanggil getAllCourses()
+     * */
+    override fun getAllCourses():ArrayList<CourseEntity> {
+        val courseResponses = remoteDataSource.getAllCourses()
+        val courseList = ArrayList<CourseEntity>()
+        for (response in courseResponses) {
+            val course = CourseEntity(response.id,
+                response.title,
+                response.description,
+                response.date,
+                false,
+                response.imagePath)
+            courseList.add(course)
+        }
+        return courseList
+    }
+
+    override fun getBookmarkedCourses(): ArrayList<CourseEntity> {
+        val courseResponses = remoteDataSource.getAllCourses()
+        val courseList = ArrayList<CourseEntity>()
+        for (response in courseResponses) {
+            val course = CourseEntity(response.id,
+                response.title,
+                response.description,
+                response.date,
+                false,
+                response.imagePath)
+            courseList.add(course)
+        }
+        return courseList
+    }
+
+    override fun getCourseWithModules(courseId: String): CourseEntity {
+        val courseResponses = remoteDataSource.getAllCourses()
+        lateinit var course: CourseEntity
+        for (response in courseResponses) {
+            if (response.id == courseId) {
+                course = CourseEntity(response.id,
+                    response.title,
+                    response.description,
+                    response.date,
+                    false,
+                    response.imagePath)
+            }
+        }
+        return course
+    }
+
+    override fun getAllModulesByCourse(courseId: String): ArrayList<ModuleEntity> {
+        val moduleResponses = remoteDataSource.getModules(courseId)
+        val moduleList = ArrayList<ModuleEntity>()
+        for(response in moduleResponses) {
+            val course = ModuleEntity(response.moduleId,
+                response.courseId,
+                response.title,
+                response.position,
+                false)
+            moduleList.add(course)
+        }
+        return moduleList
+    }
+
+    override fun getContent(courseId: String, moduleId: String): ModuleEntity {
+        val moduleResponses = remoteDataSource.getModules(courseId)
+        lateinit var module: ModuleEntity
+        for(response in moduleResponses) {
+            if (response.moduleId == moduleId) {
+                module = ModuleEntity(response.moduleId,
+                    response.courseId,
+                    response.title,
+                    response.position,
+                    false)
+                module.contentEntity = ContentEntity(remoteDataSource.getContent(moduleId).content)
+                break
+            }
+        }
+        return module
+    }
+}
