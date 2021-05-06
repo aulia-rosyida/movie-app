68378e9016fd6e68296ed84670a67ec63d41dd54
aulia-rosyida <rosyidaaulia24@gmail.com>
2021-05-06 08:47:26 +0700

    make event wrapper

---
+++ LiveDataWithAPI/app/src/main/java/com/dicoding/auliarosyida/livedatawithapi/Event.kt
@@ -0,0 +1,22 @@
+package com.dicoding.auliarosyida.livedatawithapi
+
+/**
+ * membuat event wrapper
+ * */
+open class Event<out T>(private val content: T) {
+
+    @Suppress("MemberVisibilityCanBePrivate")
+    var hasBeenHandled = false
+        private set
+
+    fun getContentIfNotHandled(): T? {
+        return if (hasBeenHandled) {
+            null
+        } else {
+            hasBeenHandled = true
+            content
+        }
+    }
+
+    fun peekContent(): T = content
+}
