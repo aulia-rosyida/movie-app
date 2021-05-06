55ace6858b0dad6b3834dfc9ca4d3203905a79b7
aulia-rosyida <rosyidaaulia24@gmail.com>
2021-05-06 21:25:14 +0700

    all data from repository dibungkus dg liveData

--- Academy/app/src/main/java/com/dicoding/auliarosyida/academy/data/source/remote/RemoteDataSource.kt
+++ Academy/app/src/main/java/com/dicoding/auliarosyida/academy/data/source/remote/RemoteDataSource.kt
@@ -1,5 +1,7 @@
 package com.dicoding.auliarosyida.academy.data.source.remote
 
+import android.os.Handler
+import android.os.Looper
 import com.dicoding.auliarosyida.academy.data.source.remote.response.ContentResponse
 import com.dicoding.auliarosyida.academy.data.source.remote.response.CourseResponse
 import com.dicoding.auliarosyida.academy.data.source.remote.response.ModuleResponse
@@ -15,7 +17,19 @@ import com.dicoding.auliarosyida.academy.utils.JsonHelper
  * */
 class RemoteDataSource private constructor(private val jsonHelper: JsonHelper) {
 
+    /**
+     * Handler dari package android.os ketika pengambilan data.
+     * Handler berfungsi untuk memberikan waktu delay sesuai dengan kebutuhan.
+     *
+     *  Handler untuk delay, seperti yang dilakukan di sini merupakah hal yang tidak disarankan.
+     *  Karena aplikasi yang kita buat hanyalah simulasi, di mana data yang diperoleh disimulasikan berasal dari server atau API.
+     *  Maka dari itu, penggunaan Handler pada aplikasi Academy digunakan untuk mensimulasikan proses asynchonous yang terjadi.
+     * */
+    private val handler = Handler(Looper.getMainLooper())
+
     companion object {
+        private const val SERVICE_LATENCY_IN_MILLIS: Long = 2000
+
         @Volatile
         private var instance: RemoteDataSource? = null
 
@@ -29,10 +43,27 @@ class RemoteDataSource private constructor(private val jsonHelper: JsonHelper) {
             }
     }
 
-    fun getAllCourses(): List<CourseResponse> = jsonHelper.loadCourses()
+    //semua data yang diambil dari Repository sampai ke ViewModel akan dibungkus dengan LiveData
+    fun getAllCourses(callback: LoadCoursesCallback) {
+        handler.postDelayed({ callback.onAllCoursesReceived(jsonHelper.loadCourses()) }, SERVICE_LATENCY_IN_MILLIS)
+    }
 
-    fun getModules(courseId: String): List<ModuleResponse> = jsonHelper.loadModule(courseId)
+    fun getModules(courseId: String, callback: LoadModulesCallback) {
+        handler.postDelayed({ callback.onAllModulesReceived(jsonHelper.loadModule(courseId)) }, SERVICE_LATENCY_IN_MILLIS)
+    }
 
-    fun getContent(moduleId: String): ContentResponse = jsonHelper.loadContent(moduleId)
+    fun getContent(moduleId: String, callback: LoadContentCallback) {
+        handler.postDelayed({ callback.onContentReceived(jsonHelper.loadContent(moduleId)) }, SERVICE_LATENCY_IN_MILLIS)
+    }
+
+    interface LoadCoursesCallback {
+        fun onAllCoursesReceived(courseResponses: List<CourseResponse>)
+    }
+    interface LoadModulesCallback {
+        fun onAllModulesReceived(moduleResponses: List<ModuleResponse>)
+    }
+    interface LoadContentCallback {
+        fun onContentReceived(contentResponse: ContentResponse)
+    }
 
 }
