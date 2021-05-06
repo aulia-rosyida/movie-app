ebfc2cbac7f7fae64a81211ee480fbc1327117e9
aulia-rosyida <rosyidaaulia24@gmail.com>
2021-05-06 21:26:22 +0700

    change return type with LiveData

--- Academy/app/src/main/java/com/dicoding/auliarosyida/academy/data/source/AcademyDataSource.kt
+++ Academy/app/src/main/java/com/dicoding/auliarosyida/academy/data/source/AcademyDataSource.kt
@@ -1,5 +1,6 @@
 package com.dicoding.auliarosyida.academy.data.source
 
+import androidx.lifecycle.LiveData
 import com.dicoding.auliarosyida.academy.data.CourseEntity
 import com.dicoding.auliarosyida.academy.data.ModuleEntity
 
@@ -8,14 +9,14 @@ import com.dicoding.auliarosyida.academy.data.ModuleEntity
  * */
 interface AcademyDataSource {
 
-    fun getAllCourses(): List<CourseEntity>
+    fun getAllCourses(): LiveData<List<CourseEntity>>
 
-    fun getBookmarkedCourses(): List<CourseEntity>
+    fun getBookmarkedCourses(): LiveData<List<CourseEntity>>
 
-    fun getCourseWithModules(courseId: String): CourseEntity
+    fun getCourseWithModules(courseId: String): LiveData<CourseEntity>
 
-    fun getAllModulesByCourse(courseId: String): List<ModuleEntity>
+    fun getAllModulesByCourse(courseId: String): LiveData<List<ModuleEntity>>
 
-    fun getContent(courseId: String, moduleId: String): ModuleEntity
+    fun getContent(courseId: String, moduleId: String): LiveData<ModuleEntity>
 
 }
