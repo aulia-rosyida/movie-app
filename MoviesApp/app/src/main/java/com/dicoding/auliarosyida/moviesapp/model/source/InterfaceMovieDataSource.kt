dc097231b257af904cb8424e97a7f4f9cc2c7914
aulia-rosyida <rosyidaaulia24@gmail.com>
2021-05-10 10:55:57 +0700

    rename interface movie data source

--- MoviesApp/app/src/main/java/com/dicoding/auliarosyida/moviesapp/model/source/InterfaceMovieDataSource.kt
+++ MoviesApp/app/src/main/java/com/dicoding/auliarosyida/moviesapp/model/source/InterfaceMovieDataSource.kt
@@ -2,7 +2,7 @@ package com.dicoding.auliarosyida.moviesapp.model.source
 
 import com.dicoding.auliarosyida.moviesapp.model.MovieEntity
 
-interface MovieDataSource {
+interface InterfaceMovieDataSource {
     fun getAllMovies(): List<MovieEntity>
 
     fun getAllTvShows(): List<MovieEntity>
