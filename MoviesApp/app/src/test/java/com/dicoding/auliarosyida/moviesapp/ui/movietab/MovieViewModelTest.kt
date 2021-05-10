4ee865c40c5bce441e1783b0a38883431ed7d769
aulia-rosyida <rosyidaaulia24@gmail.com>
2021-05-11 00:42:57 +0700

    movie dan tvshow view model test (done)

--- MoviesApp/app/src/test/java/com/dicoding/auliarosyida/moviesapp/ui/movietab/MovieViewModelTest.kt
+++ MoviesApp/app/src/test/java/com/dicoding/auliarosyida/moviesapp/ui/movietab/MovieViewModelTest.kt
@@ -1,21 +1,37 @@
 package com.dicoding.auliarosyida.moviesapp.ui.movietab
 
+import com.dicoding.auliarosyida.moviesapp.model.source.MovieRepository
+import com.dicoding.auliarosyida.moviesapp.model.source.remotesource.response.MovieResponse
+import com.dicoding.auliarosyida.moviesapp.utils.DataMovies
 import junit.framework.TestCase.assertEquals
 import junit.framework.TestCase.assertNotNull
 import org.junit.Before
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.Mockito.`when`
+import org.mockito.Mockito.verify
+import org.mockito.junit.MockitoJUnitRunner
 
+@RunWith(MockitoJUnitRunner::class)
 class MovieViewModelTest {
 
     private lateinit var viewModel: MovieViewModel
+
+    @Mock
+    private lateinit var movieRepository: MovieRepository
+
     @Before
     fun setUp() {
-        viewModel = MovieViewModel()
+        viewModel = MovieViewModel(movieRepository)
     }
-
+    
     @Test
     fun testGetMovies() {
+        `when`(movieRepository.getAllMovies()).thenReturn(DataMovies.generateMovies() as ArrayList<MovieResponse>)
         val movieEntities = viewModel.getMovies()
+        verify(movieRepository).getAllMovies()
+
         assertNotNull(movieEntities)
         assertEquals(10, movieEntities.size)
     }
--- MoviesApp/app/src/test/java/com/dicoding/auliarosyida/moviesapp/ui/tvshowtab/TvShowViewModelTest.kt
+++ MoviesApp/app/src/test/java/com/dicoding/auliarosyida/moviesapp/ui/tvshowtab/TvShowViewModelTest.kt
@@ -1,20 +1,37 @@
 package com.dicoding.auliarosyida.moviesapp.ui.tvshowtab
 
+import com.dicoding.auliarosyida.moviesapp.model.source.MovieRepository
+import com.dicoding.auliarosyida.moviesapp.model.source.remotesource.response.MovieResponse
+import com.dicoding.auliarosyida.moviesapp.utils.DataMovies
 import junit.framework.TestCase
 import org.junit.Before
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.Mockito.`when`
+import org.mockito.Mockito.verify
+import org.mockito.junit.MockitoJUnitRunner
 
+@RunWith(MockitoJUnitRunner::class)
 class TvShowViewModelTest {
 
     private lateinit var viewModel: TvShowViewModel
+
+    @Mock
+    private lateinit var movieRepository: MovieRepository
+
     @Before
     fun setUp() {
-        viewModel = TvShowViewModel()
+        viewModel = TvShowViewModel(movieRepository)
     }
 
     @Test
     fun testGetTvShows() {
+        `when`(movieRepository.getAllTvShows()).thenReturn(DataMovies.generateTvShows() as ArrayList<MovieResponse>)
         val tvShowEntities = viewModel.getTvShows()
+        verify(movieRepository).getAllTvShows()
+
         TestCase.assertNotNull(tvShowEntities)
         TestCase.assertEquals(10, tvShowEntities.size)
     }
