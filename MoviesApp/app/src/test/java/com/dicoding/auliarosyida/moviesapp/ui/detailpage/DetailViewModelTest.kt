f9ff76332594ebb242ed74df05f623d305515d80
aulia-rosyida <rosyidaaulia24@gmail.com>
2021-05-11 01:09:03 +0700

    update unit testing detail view model

--- MoviesApp/app/src/test/java/com/dicoding/auliarosyida/moviesapp/ui/detailpage/DetailViewModelTest.kt
+++ MoviesApp/app/src/test/java/com/dicoding/auliarosyida/moviesapp/ui/detailpage/DetailViewModelTest.kt
@@ -1,28 +1,41 @@
 package com.dicoding.auliarosyida.moviesapp.ui.detailpage
 
+import com.dicoding.auliarosyida.moviesapp.model.source.MovieRepository
 import com.dicoding.auliarosyida.moviesapp.utils.DataMovies
 import junit.framework.TestCase.assertEquals
 import junit.framework.TestCase.assertNotNull
 import org.junit.Before
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.Mockito.`when`
+import org.mockito.Mockito.verify
+import org.mockito.junit.MockitoJUnitRunner
 
+@RunWith(MockitoJUnitRunner::class)
 class DetailViewModelTest {
 
     private lateinit var viewModel: DetailViewModel
     private val dummyMovie = DataMovies.generateMovies()[0]
     private val dummyTvShow = DataMovies.generateTvShows()[0]
-    private val tempId = dummyMovie.id
+    private val tempMovieId = dummyMovie.id
+    private val tempTvShowId = dummyTvShow.id
+
+    @Mock
+    private lateinit var movieRepository: MovieRepository
 
     @Before
     fun setUp() {
-        viewModel = DetailViewModel()
-        viewModel.setSelectedDetail(tempId)
+        viewModel = DetailViewModel(movieRepository)
+        viewModel.setSelectedDetail(tempMovieId)
     }
 
     @Test
     fun testGetMovieEntity() {
         viewModel.setSelectedDetail(dummyMovie.id)
+        `when`(movieRepository.getDetailMovie(tempMovieId)).thenReturn(dummyMovie)
         val movieEntity = viewModel.getEntity()
+        verify(movieRepository).getDetailMovie(tempMovieId)
 
         assertNotNull(movieEntity)
         assertEquals(dummyMovie.id, movieEntity.id)
@@ -39,8 +52,11 @@ class DetailViewModelTest {
 
     @Test
     fun testGetTvShowEntity() {
+
         viewModel.setSelectedDetail(dummyTvShow.id)
+        `when`(movieRepository.getDetailTvShow(tempTvShowId)).thenReturn(dummyTvShow)
         val tvShowEntity = viewModel.getEntity()
+        verify(movieRepository).getDetailTvShow(tempTvShowId)
 
         assertNotNull(tvShowEntity)
         assertEquals(dummyTvShow.id, tvShowEntity.id)
